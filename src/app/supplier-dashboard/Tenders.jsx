0296bd17b9eb8d75841359361c42be8313adda14
deepakguptabvp <deepakgupta.150743@gmail.com>
2025-05-24 20:48:18 +0530

    bug

--- src/app/supplier-dashboard/Tenders.jsx
+++ src/app/supplier-dashboard/Tenders.jsx
@@ -21,8 +21,7 @@ export default function TendersPage({ setCreateBid, setActiveSection }) {
   }, []);
 
   useEffect(() => {
-    const filteredTenders = sampleTenders
-      ?.filter(
+    const filteredTenders = sampleTenders?.filter(
         (t) =>
           (!filters.search ||
             t.name.toLowerCase().includes(filters.search.toLowerCase())) &&
