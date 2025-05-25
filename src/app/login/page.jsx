497362c4d314ca2336a169212b01df4323cc1b72
deepakguptabvp <deepakgupta.150743@gmail.com>
2025-05-25 16:10:36 +0530

    Implemented dashboard-selector and login otp based component.

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,131 @@
-import React from "react";
+"use client";
+import React, { use, useEffect, useState } from "react";
+// import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
+import Link from "next/link";
+
 
 function Login() {
+  const [phone, setPhone] = useState("");
+  const [otp, setOtp] = useState("");
+  const [error, setError] = useState("");
+  const [otpSent, setOtpSent] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [sdkLoaded, setSdkLoaded] = useState(false);
+  const [isResendDisabled, setIsResendDisabled] = useState(false);
+  const [resendTimer, setResendTimer] = useState(60);
+
+  useEffect(() => {
+    if (!sdkLoaded) {
+      const script = document.createElement("script");
+      script.id = "otpless-sdk";
+      script.src = "https://otpless.com/v4/headless.js";
+      script.setAttribute("data-appid", "E3Q3X8BESP4COB9TORA6");
+      script.onload = () => {
+        setSdkLoaded(true);
+        initializeOtpless();
+      };
+      document.head.appendChild(script);
+
+      return () => {
+        document.head.removeChild(script);
+      };
+    }
+  }, [sdkLoaded]);
+
+  const initializeOtpless = () => {
+    if (!window.OTPless) return;
+
+    const callback = (event) => {
+      const handlers = {
+        OTP_AUTO_READ: () => {
+          const { otp } = event.response;
+          setOtp(otp);
+        },
+        ONETAP: () => {
+          console.log("Token:", event.response.token);
+        },
+        FAILED: () => console.error("Authentication Failed"),
+        FALLBACK_TRIGGERED: () => console.warn("Fallback Triggered"),
+      };
+
+      if (handlers[event.responseType]) handlers[event.responseType]();
+    };
+
+    window.OTPlessSignin = new window.OTPless(callback);
+  };
+
+  const initiateAuth = () => {
+    if (!window.OTPlessSignin) return;
+    if (!phone.trim()) return toast("Please enter a phone number.");
+
+    window.OTPlessSignin.initiate({
+      channel: "PHONE",
+      phone,
+      countryCode: "+91",
+    });
+
+    toast("OTP sent to your phone");
+    setOtpSent(true);
+    setIsResendDisabled(true);
+    setResendTimer(60);
+    startResendTimer();
+  };
+
+  // const navigate = useNavigate();
+  const handleOtpVerification = async () => {
+    try {
+      const { data } = await axios.post("user/otp-verified", { phone });
+      setUser(data.user);
+      localStorage.setItem("BidA2ZUser", data.token);
+      setError("");
+      // navigate to buyer / supplier page.
+     <Link href="/dashboard-selector"></Link>
+     
+    } catch {
+      setError("Something went wrong! Try Again");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const verifyOTP = async () => {
+    setLoading(true);
+    const res = await window.OTPlessSignin.verify({
+      channel: "PHONE",
+      phone,
+      otp,
+      countryCode: "+91",
+    });
+
+    if (res.success) {
+      handleOtpVerification();
+  
+      //  <Link href='/dashboard-selector'>Dashboard Selector</Link>
+    } else {
+      setError(
+        res.statusCode === 400 ? "Invalid OTP!" : "Something went wrong!"
+      );
+      setLoading(false);
+    }
+  };
+
+  const startResendTimer = () => {
+    let timeLeft = 60;
+    const interval = setInterval(() => {
+      timeLeft -= 1;
+      setResendTimer(timeLeft);
+      if (timeLeft <= 0) {
+        clearInterval(interval);
+        setIsResendDisabled(false);
+      }
+    }, 1000);
+  };
+
   return (
-    <div className="flex bg-gray-100 items-center justify-center">
+    <div className="flex bg-gray-100 items-center justify-center text-black">
       <div className="flex flex-col md:flex-row w-full max-w-7xl shadow-2xl overflow-hidden">
         {/* Left Side - Image */}
         <div className="w-full md:w-3/5 p-6 md:p-8 bg-white flex items-center justify-center">
@@ -25,36 +148,74 @@ function Login() {
               </p>
             </div>
 
-            <form className="space-y-6">
-              <div className="space-y-4">
+            <div className="space-y-4">
+              <div className="relative">
                 <input
-                  type="email"
-                  placeholder="Enter your email"
-                  className="w-full px-4 py-3 rounded-lg bg-black/10 border border-white/20 text-black placeholder-black/70 focus:outline-none focus:ring-2 focus:ring-white/50"
+                  type="text"
+                  placeholder="Phone Number *"
+                  value={phone}
+                  onChange={(e) => {
+                    if (e.target.value.length <= 10) setPhone(e.target.value);
+                  }}
+                  className="w-full border border-gray-300 px-4 py-2 rounded-md pl-12 focus:ring-2 focus:ring-blue-500 outline-none"
                 />
-                <input
-                  type="password"
-                  placeholder="Enter your password"
-                  className="w-full px-4 py-3 rounded-lg bg-black/10 border border-white/20 text-black placeholder-black/70 focus:outline-none focus:ring-2 focus:ring-white/50"
-                />
+                <span className="absolute left-0 bg-teal-500 px-2 h-full rounded-l-md py-2 text-white">
+                  +91
+                </span>
               </div>
 
-              <button
-                type="submit"
-                className="w-full py-3 px-6 hover:bg-black text-white rounded-lg font-semibold bg-black/70 transition duration-200 cursor-pointer"
-              >
-                LOG IN
-              </button>
-            </form>
+              {otpSent ? (
+                <>
+                  <input
+                    type="text"
+                    placeholder="Enter OTP *"
+                    value={otp}
+                    onChange={(e) => setOtp(e.target.value)}
+                    className="w-full border border-gray-300 px-4 py-2 rounded-md focus:ring-2 focus:ring-blue-500 outline-none"
+                  />
+                  <button
+                    type="button"
+                    onClick={verifyOTP}
+                    disabled={loading}
+                    className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition"
+                  >
+                    {loading ? "Verifying..." : "Verify OTP"}
+                  </button>
+                </>
+              ) : (
+                <button
+                  type="button"
+                  onClick={initiateAuth}
+                  className="w-full bg-teal-600 text-white py-2 rounded-md hover:bg-teal-700 transition"
+                >
+                  Request OTP
+                </button>
+              )}
 
-            <div className="text-center space-y-4">
+              {otpSent && (
+                <div className="text-center mt-2">
+                  {isResendDisabled ? (
+                    <p className="text-gray-500">{`Resend OTP in ${resendTimer}s`}</p>
+                  ) : (
+                    <button
+                      onClick={initiateAuth}
+                      className="text-blue-500 underline bg-gray-100 px-2 py-1 rounded"
+                    >
+                      Resend OTP
+                    </button>
+                  )}
+                </div>
+              )}
+            </div>
+
+            {/* <div className="text-center space-y-4">
               <p className="text-black/90">Don't have an account?</p>
               <button className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium  group  hover:bg-black text-white rounded-lg  bg-black/70 ">
                 <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-transparent rounded-md group-hover:bg-transparent cursor-pointer">
                   SIGN UP
                 </span>
               </button>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
