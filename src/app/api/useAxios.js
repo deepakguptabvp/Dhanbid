65953a0bb677c98166a7cb9f5b1d975ffc9c87fa
deepakguptabvp <deepakgupta.150743@gmail.com>
2025-06-23 21:55:50 +0530

    footer light theme

--- src/app/api/useAxios.js
+++ src/app/api/useAxios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosAPI = () => {
   const token = localStorage.getItem("DBToken");
   const baseURL =
-    window.location.hostname!=="localhost"
+    window.location.hostname !== "localhost"
       ? "https://dhanbid-server.vercel.app/"
       : "http://localhost:5000/api";
 
